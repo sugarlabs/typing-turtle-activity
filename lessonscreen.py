sugarlabs/typing-turtle-activity

commit 62baf1e4989ae8932d67b67e97dca451b4f45bd1

Port to Python 3

lessonscreen.py
@@ -31,7 +31,7 @@ from sugar3.graphics import *
 import keyboard, medalscreen
 
 # Paragraph symbol unicode character.
-PARAGRAPH_CODE = u'\xb6'
+PARAGRAPH_CODE = '\xb6'
 
 # Maximium width of a text line in text lesson mode.
 LINE_WIDTH = 70
@@ -213,7 +213,7 @@ class LessonScreen(Gtk.VBox):
         self.advance_step()
 
     def wrap_line(self, line):
-        r = re.compile('(\W+)', re.UNICODE)
+        r = re.compile(r'(\W+)', re.UNICODE)
         words = r.split(line)
         
         new_lines = []
@@ -246,9 +246,9 @@ class LessonScreen(Gtk.VBox):
             step = self.lesson['steps'][self.next_step_idx]
             self.next_step_idx = self.next_step_idx + 1
             
-            self.text = unicode(step['text'])
-            self.instructions = unicode(step['instructions'])
-            if step.has_key('mode'):
+            self.text = str(step['text'])
+            self.instructions = str(step['instructions'])
+            if 'mode' in step:
                 self.mode = step['mode']
             else:
                 if len(self.text) == 1:
@@ -321,7 +321,7 @@ class LessonScreen(Gtk.VBox):
 
                 # Add the text to copy.
                 self.lessonbuffer.insert_with_tags_by_name(
-                    self.lessonbuffer.get_end_iter(), l.encode('utf-8') + '\n', 'text')
+                    self.lessonbuffer.get_end_iter(), l.decode('utf-8') + '\n', 'text')
                 
                 # Leave a marker where we will later insert text.
                 self.line_marks[line_idx] = self.lessonbuffer.create_mark(None, self.lessonbuffer.get_end_iter(), True)
@@ -586,7 +586,7 @@ class LessonScreen(Gtk.VBox):
             # Compare this medal with any existing medals for this lesson.
             # Only record the best one.
             add_medal = True
-            if self.activity.data['medals'].has_key(lesson_name):
+            if lesson_name in self.activity.data['medals']:
                 old_medal = self.activity.data['medals'][lesson_name]
 
                 order = ' '.join([m['name'] for m in medals])

activity/activity.info
@@ -4,7 +4,7 @@ activity_version = 31
 license = GPLv3+
 bundle_id = org.laptop.community.TypingTurtle
 icon = Activity-typingturtle
-exec = sugar-activity typingturtle.TypingTurtle
+exec = sugar-activity3 typingturtle.TypingTurtle
 summary = Need some help typing? Here you will learn the best way to hold your hands in order for you to become a faster typist!
 repository = https://github.com/sugarlabs/typing-turtle-activity
 tags = Language;System;Game

balloongame.py
@@ -19,6 +19,9 @@ import random, datetime
 
 from gettext import gettext as _
 
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('PangoCairo', '1.0')
 from gi.repository import Gtk
 from gi.repository import Gdk
 from gi.repository import GObject
@@ -109,7 +112,10 @@ class BalloonGame(Gtk.VBox):
     def stop_cb(self, widget):
         # Stop the animation loop.
         if self.update_timer:
-            GObject.source_remove(self.update_timer)
+            try:
+                GObject.source_remove(self.update_timer)
+            except:
+                pass # Try remove instance, if not found, just pass
         
         self.activity.pop_screen()
 
@@ -120,7 +126,7 @@ class BalloonGame(Gtk.VBox):
 
         # Extract information about the key pressed.
         key = Gdk.keyval_to_unicode(event.keyval)
-        if key != 0: key = unichr(key)
+        if key != 0: key = chr(key)
 
         if self.finished:
             key_name = Gdk.keyval_name(event.keyval)
@@ -221,8 +227,8 @@ class BalloonGame(Gtk.VBox):
         fd = Pango.FontDescription('Serif Bold')
         fd.set_size(16 * Pango.SCALE)
         pango_layout.set_font_description(fd)
-        pango_layout.set_text(title.encode('utf-8'),
-                              len(title.encode('utf-8')))
+        pango_layout.set_text(title,
+                              len(title))
         size = pango_layout.get_size()
         tx = x + (w / 2) - (size[0] / Pango.SCALE) / 2
         ty = y + 100
@@ -282,7 +288,7 @@ class BalloonGame(Gtk.VBox):
             # Compare this medal with any existing medals for this lesson.
             # Only record the best one.
             add_medal = True
-            if self.activity.data['medals'].has_key(self.lesson['name']):
+            if self.lesson['name'] in self.activity.data['medals']:
                 old_medal = self.activity.data['medals'][self.lesson['name']]
 
                 order = ' '.join([m['name'] for m in medals])

editlessonlistscreen.py
@@ -20,7 +20,8 @@
 import logging, os, math, time, copy, locale, datetime, random, re
 from gettext import gettext as _
 import json
-
+import gi
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 from gi.repository import GObject
 

editlessonscreen.py
@@ -366,7 +366,7 @@ class EditLessonScreen(Gtk.VBox):
         if self.lesson['type'] == 'normal':
             self.has_normal_widgets = True
             
-            if not self.lesson.has_key('steps') or len(self.lesson['steps']) == 0:
+            if 'steps' not in self.lesson or len(self.lesson['steps']) == 0:
                 step = { 'instructions': '', 'text': '' }
                 self.lesson['steps'] = [ step ]              
                   
@@ -381,7 +381,7 @@ class EditLessonScreen(Gtk.VBox):
         if self.lesson['type'] == 'balloon':
             self.has_balloon_widgets = True
 
-            if not self.lesson.has_key('words') or len(self.lesson['words']) == 0:
+            if 'words' not in self.lesson or len(self.lesson['words']) == 0:
                 self.lesson['words'] = []
             
             textlabel = Gtk.Label()
@@ -447,7 +447,7 @@ class EditLessonScreen(Gtk.VBox):
         self.lesson['description'] = buf.get_text(buf.get_start_iter(),
                                                   buf.get_end_iter(), False)
         
-        if not self.lesson.has_key('options'):
+        if 'options' not in self.lesson:
             self.lesson['options'] = {}
         self.lesson['options']['mistakes'] = self.mistakescheck.get_active()
         self.lesson['options']['backspace'] = self.backspacecheck.get_active()
@@ -561,7 +561,7 @@ class EditLessonScreen(Gtk.VBox):
             if self.lesson['type'] == 'balloon':
                 self.lesson['words'] = lessonbuilder.build_game_words(length, new_keys, known_keys, words, [])                
                 
-        except Exception, e:
+        except Exception as e:
             logging.error('Unable to generate lesson: ' + str(e))
 
         self.build()

keyboard.py
@@ -19,7 +19,8 @@
 import cairo
 import copy
 import os, glob, re
-
+import gi
+gi.require_version('PangoCairo', '1.0') # Check PangoCairo Version to prevent warnings
 from gi.repository import Gtk
 from gi.repository import Pango
 from gi.repository import PangoCairo
@@ -27,7 +28,7 @@ from gi.repository import Gdk
 from gi.repository import GObject
 from gi.repository import GdkPixbuf
 
-import StringIO
+import io
 import json
 import subprocess
 from layouts.olpc import OLPC_LAYOUT
@@ -37,7 +38,7 @@ from layouts.olpcm import OLPCM_LAYOUT
 HAND_YOFFSET = -15
 
 # Unicode symbol for the paragraph key.
-PARAGRAPH_CODE = u'\xb6'
+PARAGRAPH_CODE = '\xb6'
 
 # List of all key properties in the keyboard layout description.
 #
@@ -108,7 +109,8 @@ def _is_olpcm_model():
     code = None
     p = subprocess.Popen(["setxkbmap", "-query"], stdout=subprocess.PIPE)
     out, err = p.communicate()
-    for line in out.splitlines():
+    
+    for line in out.decode('utf-8').splitlines():
         if line.startswith('model:'):
             code = line.split()[1]
     return code == 'olpcm'
@@ -128,7 +130,7 @@ class KeyboardImages:
         self.images = {}
 
     def get_image(self, name):
-        if not self.images.has_key(name):
+        if name not in self.images:
             self.load_image(name)
 
         return self.images[name]
@@ -157,7 +159,9 @@ class KeyboardData:
         self.letter_map = {}
         
         # Access the current GTK keymap.
-        self.keymap = Gdk.Keymap.get_default()
+        # self.keymap = Gdk.Keymap.get_default()
+        # Removed above line due to Gtk3 deprecation
+        self.keymap = Gdk.Keymap.get_for_display(Gdk.Display.get_default())
 
     def set_layout(self, layout): 
         self._build_key_list(layout)
@@ -187,10 +191,10 @@ class KeyboardData:
                 # defaults, in that order.
                 for p in KEY_PROPS:
                     pname = p['name']
-                    if not key.has_key(pname):
-                        if g.has_key(pname):
+                    if pname not in key:
+                        if pname in g:
                             key[pname] = g[pname]
-                        elif layout.has_key(pname):
+                        elif pname in layout:
                             key[pname] = layout[pname]
                         else:
                             key[pname] = p['default']
@@ -289,7 +293,7 @@ class KeyboardData:
         best_score = 3
         best_result = None
         
-        for sig, l in self.letter_map.items():
+        for sig, l in list(self.letter_map.items()):
             if l == letter:
                 scan, state, group = self.parse_key_sig(sig)
                 
@@ -326,14 +330,14 @@ class KeyboardData:
 
     def get_letter_for_key_state_group(self, key, state, group):
         sig = self.format_key_sig(key['key-scan'], state, group)
-        if self.letter_map.has_key(sig):
+        if sig in self.letter_map:
             return self.letter_map[sig]
         else:
             success, keyval, effective_group, level, consumed_modifiers = \
                 self.keymap.translate_keyboard_state(
                     key['key-scan'], self.active_state, self.active_group)
             if success:
-                return unichr(Gdk.keyval_to_unicode(keyval)).encode('utf-8')
+                return chr(Gdk.keyval_to_unicode(keyval)).encode('utf-8')
 
         return ''
 
@@ -367,7 +371,7 @@ class KeyboardWidget(KeyboardData, Gtk.DrawingArea):
         self.hilite_letter = None
         
         self.draw_hands = False
-        
+        # TODO FIXME Gdk.Color.Parse() Deprecation
         self.modify_bg(Gtk.StateType.NORMAL, Gdk.Color.parse('#d0d0d0')[1])
 
         # Connect keyboard grabbing and releasing callbacks.        
@@ -449,7 +453,10 @@ class KeyboardWidget(KeyboardData, Gtk.DrawingArea):
         fd = Pango.FontDescription('Monospace')
         fd.set_size(10 * Pango.SCALE)
         pango_layout.set_font_description(fd)
-        pango_layout.set_text(text, len(text))
+        if type(text) is bytes:
+        	pango_layout.set_text(text.decode('utf-8'), len(text.decode('utf-8')))
+        else:
+            pango_layout.set_text(text, len(text))
 
         cr.move_to(x1 + 8, y2 - 23)
         PangoCairo.update_layout(cr, pango_layout)
@@ -535,7 +542,7 @@ class KeyboardWidget(KeyboardData, Gtk.DrawingArea):
 
         if event.string:
             sig = self.format_key_sig(event.hardware_keycode, state, event.group)
-            if not self.letter_map.has_key(sig):
+            if sig not in self.letter_map:
                 self.letter_map[sig] = event.string
                 self.queue_draw()
 
@@ -552,7 +559,7 @@ class KeyboardWidget(KeyboardData, Gtk.DrawingArea):
     def set_draw_hands(self, enable):
         self.draw_hands = enable
         self.queue_draw()
-
+    
     def get_key_pixbuf(self, key, state=0, group=0, scale=1):
         w = int(key['key-width'] * scale)
         h = int(key['key-height'] * scale)
@@ -574,12 +581,14 @@ class KeyboardWidget(KeyboardData, Gtk.DrawingArea):
         self._draw_key(key, cr)
 
         # Convert cairo.Surface to Pixbuf
-        pixbuf_data = StringIO.StringIO()
+        pixbuf_data = io.BytesIO()
         surface.write_to_png(pixbuf_data)
         pxb_loader = GdkPixbuf.PixbufLoader.new_with_type('png')
         pxb_loader.write(pixbuf_data.getvalue())
         temp_pix = pxb_loader.get_pixbuf()
         pxb_loader.close()
+        
+        	
 
         self.active_state, self.active_group = old_state, old_group
 

keybuilder.py
@@ -21,7 +21,8 @@ import keyboard
 
 from gi.repository import Gtk
 
-window = Gtk.Window(Gtk.WindowType.TOPLEVEL)
+# window = Gtk.Window(Gtk.WindowType.TOPLEVEL) Removed above line to because of GtkDeprecation of positional arguments
+window = Gtk.Window(type=Gtk.WindowType.TOPLEVEL)
 window.set_title("keyboard widget")
 window.connect("destroy", lambda w: Gtk.main_quit())
 window.show_all()

lessonbuilder.py
@@ -31,8 +31,8 @@ from gi.repository import Gtk
 import keyboard
 
 def error(s):
-    print "lessonbuilder: ERROR: ", s 
-    print "The lesson could not be generated, exiting.\n\n"
+    print(("lessonbuilder: ERROR: ", s)) 
+    print("The lesson could not be generated, exiting.\n\n")
     sys.exit(1)
 
 def make_all_triples(keys):
@@ -49,14 +49,14 @@ def make_all_doubles(keys):
 
 def make_random_triples(keys, count):
     text = ''
-    for y in xrange(0, count):
+    for y in range(0, count):
         k = random.choice(keys)
         text += k + k + ' ' + k + ' '
     return text.strip()
 
 def make_random_doubles(keys, count):
     text = ''
-    for y in xrange(0, count):
+    for y in range(0, count):
         k = random.choice(keys)
         text += k + k + ' '
     return text.strip()
@@ -65,7 +65,7 @@ def make_jumbles(required_keys, keys, count, width):
     text = ''
     for y in range(0, count):
         # Alternating between required and non-required.  Is this too challenging to type?
-        for x in range(0, width/2):
+        for x in range(0, int(width/2)):
             text += random.choice(required_keys)
             text += random.choice(keys)
         text += ' '
@@ -82,7 +82,7 @@ def make_all_pairs(keys):
 
 def make_random_pairs(required_keys, keys, count):
     text = ''
-    for y in xrange(0, count):
+    for y in range(0, count):
         k1 = random.choice(required_keys)
         k2 = random.choice(keys)
         text += random.choice([k1 + k2, k2 + k1]) + ' '
@@ -97,11 +97,11 @@ def make_all_joined_pairs(keys1, keys2):
             text += k2 + k1 + ' '
     return text.strip()
 
-RE_WHITESPACE = re.compile('\s+', re.UNICODE)
+RE_WHITESPACE = re.compile(r'\s+', re.UNICODE)
 
 def load_wordlist(path):
     try:
-        text = unicode(open(path, 'r').read())
+        text = str(open(path, 'r').read())
         
         # Split words by whitespace characters.
         # This preserves partial punctuation in some words, which is
@@ -117,13 +117,13 @@ def load_wordlist(path):
         return []
 
 def get_pairs_from_wordlist(words):
-    print 'Calculating common pairs...'
+    print('Calculating common pairs...')
 
     # Construct char_map, a map for each character c0 in words, giving the frequency of each other
     # character c1 in words following c0.
     char_map = {}
     for word in words:
-        for i in xrange(0, len(word)-1):
+        for i in range(0, len(word)-1):
             c0 = word[i]
             c1 = word[i+1]
             
@@ -133,8 +133,8 @@ def get_pairs_from_wordlist(words):
 
     # Convert to list of pairs with probability.    
     pairs = []
-    for c0, c0_map in char_map.items():
-        for c1, c1_value in c0_map.items():
+    for c0, c0_map in list(char_map.items()):
+        for c1, c1_value in list(c0_map.items()):
             pairs.append((c0+c1, c1_value))
 
     # Sort by frequency.
@@ -164,7 +164,7 @@ def filter_pairs(pairs, required_keys, keys):
     total = 0.0
     for p in good_pairs:
         total += p[1]
-    good_pairs = [(p[0], p[1]/total) for p in good_pairs]
+    good_pairs = [(p[0], int(p[1]/total)) for p in good_pairs]
 
     return good_pairs
     
@@ -187,13 +187,13 @@ def make_weighted_wordlist_pairs(pairs, required_keys, keys, count):
         return make_random_pairs(required_keys, keys, count)
         
     text = ''
-    for y in xrange(0, count):
+    for y in range(0, count):
         p = get_weighted_random_pair(good_pairs)
         text += p[0] + ' '
     return text.strip()
     
 def filter_wordlist(words, all_keys, req_keys, minlen, maxlen, bad_words):
-    print 'Filtering word list...'
+    print('Filtering word list...')
 
     # Uniquify words.
     # TODO: Build a frequency table as with the pairs.
@@ -417,7 +417,7 @@ def build_text_step(path):
     instructions = _('Copy out the following text.')
 
     try:
-        text = unicode(open(path, 'r').read())
+        text = str(open(path, 'r').read())
     except:
         text = ''
 
@@ -512,14 +512,14 @@ def main():
         bad_words = load_wordlist(options.badwordlist)
 
     # Convert string arguments to Unicode.
-    options.name = unicode(options.name)
-    options.keys = unicode(options.keys)
-    options.base_keys = unicode(options.base_keys)
-    options.desc = unicode(options.desc.replace('\\n', '\n'))
+    options.name = str(options.name)
+    options.keys = str(options.keys)
+    options.base_keys = str(options.base_keys)
+    options.desc = str(options.desc.replace('\\n', '\n'))
     
     random.seed(options.seed)
 
-    print "Building lesson '%s'..." % options.name
+    print(("Building lesson '%s'..." % options.name))
 
     lesson = {}
     lesson['name'] = options.name
@@ -558,15 +558,16 @@ def main():
         lesson['words'] = build_game_words(
             count=options.length, new_keys=options.keys, base_keys=options.base_keys, 
             words=words, bad_words=bad_words)
-
+    
     text = json.dumps(lesson, ensure_ascii=False, sort_keys=True, indent=4)
 
-    open(options.output, 'w').write(text)
+    with open(options.output, 'w') as x:
+        x.write(text)
 
 if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print "Ctrl-C detected, aborting."
+        print("Ctrl-C detected, aborting.")
         sys.exit(1)
 

commit f7e503c5df9fcfa87af81f194d950375b6658cdf

Fix AttributeError, regression introduced by 62baf1e4
Signed-off-by: Ibiam Chihurumnaya <ibiamchihurumnaya@gmail.com>

lessonscreen.py
@@ -321,7 +321,7 @@ class LessonScreen(Gtk.VBox):
 
                 # Add the text to copy.
                 self.lessonbuffer.insert_with_tags_by_name(
-                    self.lessonbuffer.get_end_iter(), l.decode('utf-8') + '\n', 'text')
+                    self.lessonbuffer.get_end_iter(), l + '\n', 'text')
                 
                 # Leave a marker where we will later insert text.
                 self.line_marks[line_idx] = self.lessonbuffer.create_mark(None, self.lessonbuffer.get_end_iter(), True)